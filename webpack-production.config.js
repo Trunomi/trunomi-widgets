98d4be42d12132027babc8b94e0c77fb59458473
Luis Serra Garcia <lserraga@ucsc.edu>
2018-05-23 18:32:43 +0100

    HotFix

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: ['./app/index.js'],
+    entry: ['babel-polyfill', './app/index.js'],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.min.js',
