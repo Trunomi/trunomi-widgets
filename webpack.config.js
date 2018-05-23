1d7eccb129eb35332b210e4ad799cc25e92aaa56
Luis Serra Garcia <lserraga@ucsc.edu>
2018-05-23 18:24:34 +0100

    HotFix

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 var devConfig = require('./webpack-dev.config');
 var productionConfig = require('./webpack-production.config');
+var demoConfig = require('./webpack-demo.config');
 
 module.exports = function(env) {
-    if(env && (env.dev || env.demo)) {
+    if(env && env.dev) {
         return devConfig
     }
+    else if (env && env.demo)
+    	return demoConfig
     else {
         return productionConfig
     }
