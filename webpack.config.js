6431093ee282e0167531f272081f501aedd87e93
Luis Serra Garcia <lserraga@ucsc.edu>
2018-05-24 12:30:58 +0100

    Deployment fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,10 @@
 var devConfig = require('./webpack-dev.config');
 var productionConfig = require('./webpack-production.config');
-var demoConfig = require('./webpack-demo.config');
 
 module.exports = function(env) {
-    if(env && env.dev) {
+    if(env && (env.dev || env.demo)) {
         return devConfig
     }
-    else if (env && env.demo)
-    	return demoConfig
     else {
         return productionConfig
     }
