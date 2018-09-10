d19862a4c4e46bb83eff03bd8ffc3e3ef43c5a20
Luis Serra Garcia <lserraga@ucsc.edu>
2018-09-10 10:35:39 +0100

    Fixed loading icon

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import * as BS from 'react-bootstrap';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 export const LoadingInline = () => {
     return <div className='loadingContainer'>
-        <div className='loadingRow'>
-            <i className='icon-spin1 animate-spin loadingIcon' style={{fontSize: '75px'}}/>
+        <div className='loadingRow' style={{color: '#1a1a1a'}}>
+            <CircularProgress size={100} color='inherit'/>
         </div>
     </div>
 };
@@ -19,7 +20,9 @@ class LoadingModal extends React.Component{
                 show={loading}
                 container={this}
                 aria-labelledby="contained-modal-title">
-                <i className='icon-spin1 animate-spin loadingIcon'/>
+                <div style={{color: '#1a1a1a'}}>
+                    <CircularProgress size={100} color='inherit'/>
+                </div>
             </BS.Modal>
             {children}
         </div>
